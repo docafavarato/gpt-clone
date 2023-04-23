44dd8bad1ed007a1190b45a5136e9d39941a056f
João Pedro Favarato <98183878+docafavarato@users.noreply.github.com>
2023-04-23 11:02:38 -0300

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import urllib.request
 import os
 
-openai.api_key = "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"
+openai.api_key = "YOUR_API_KEY"
 def ask(question):
     completions = openai.Completion.create(
         engine="text-davinci-003",
